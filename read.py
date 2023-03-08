341eaa89c937f0bb661a61a01086068bb226ef8f
Matehw <77369415+Matehw@users.noreply.github.com>
2023-03-08 21:33:03 +0800

    add hegal rp
    123

--- read.py
+++ read.py
@@ -4,3 +4,6 @@ https://arxiv.org/pdf/1907.13337.pdf
 #https://github.com/jzhou316/Unsupervised-Sentence-Summarization
 
 https://github.com/raphael-sch/HC_Sentence_Summarization
+
+hegal
+https://github.com/hpzhang94/hegel_sum
